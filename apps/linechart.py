cfc70152597601649f742745ca100cdc4d5a9326
Pravin <pravin22jd@gmail.com>
2018-08-04 20:07:57 +0530

    first version of multiple pages app

---
+++ apps/linechart.py
@@ -0,0 +1,24 @@
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+from apps import commonmodules
+
+from app import app
+
+layout = html.Div([
+    commonmodules.get_header(),
+    commonmodules.get_menu(),   
+    html.H3('Line Chart'),    
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'line', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'line', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    ),
+])
